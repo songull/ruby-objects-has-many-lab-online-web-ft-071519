4d488678f2917cb35e820d72bccb196c713962bf
Kyler Barber <kylerbarber820@yahoo.com>
2019-08-13 23:09:09 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,11 @@
 class Post
+  attr_accessor :author, :title
 
-  attr_accessor :title, :author
-  def initialize(title)
+   def initialize(title)
     @title = title
   end
 
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+   def author_name
+    self.author ? self.author.name : nil
   end
-end
+end 
