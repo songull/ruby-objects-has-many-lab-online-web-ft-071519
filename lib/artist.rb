e863a0f359d45ab332b511b3c6016d58cfbfeffc
Kyler Barber <kylerbarber820@yahoo.com>
2019-08-13 23:15:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,28 @@
 class Artist
 
-  attr_accessor :name
+	attr_accessor :name, :songs
 
-@count = 1
+	@@all = []
 
-  def initialize(name)
-    @name = name
-    @songs = ["Adele"]
-  end
+	def initialize(name)
+		@name = name
+		@songs = []
+		@@all << self
+	end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+	def add_song(song)
+		@songs << song
+		song.artist = self
+	end
 
-  def songs
-    @songs
-  end
+	def add_song_by_name(song_name)
+		song = Song.new(song_name)
+		add_song(song)
+	end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-  song.artist = self
-  end
+	def self.song_count
+		Song.all.length
+	end
 
-def self.song_count
- @count += 1
-end
-
-end
+end 
+  24  lib/author.rb 
