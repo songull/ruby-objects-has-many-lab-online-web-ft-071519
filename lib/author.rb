3818fd33b89c6d1537ccba273d429998a2f2ebbd
Kyler Barber <kylerbarber820@yahoo.com>
2019-08-13 23:09:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,31 @@
 class Author
+  attr_accessor :name
 
-  attr_accessor :name, :posts
+   @@post_count = 0
 
-  @@post_count = 0
-
-  def initialize(name)
+   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+   def posts
+    @posts
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def self.post_count
+   def self.post_count
     @@post_count
   end
-end
+
+   def add_post(post_title)
+    @posts << post_title
+    post_title.author = self
+    @@post_count += 1
+  end
+
+   def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+end 
